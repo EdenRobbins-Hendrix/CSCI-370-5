71a1cced902e5fe9b08fbc40df4001ddd9c4438b
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-03-21 21:18:39 -0500

    Added in some functionality to prototypes
    Player should move, camera should follow. Exact values to be tweaked.

--- Lab 5/Assets/Scripts/PlayerMovement.cs
+++ Lab 5/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float horizontal;
+    float vertical;
+    Rigidbody2D self;
+    Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        self = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Replace this with animator based movement later on
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        self.linearVelocity = new Vector2(horizontal, vertical);
     }
 }
