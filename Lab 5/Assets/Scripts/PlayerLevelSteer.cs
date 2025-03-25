6155de7aacd85f1de2b79050c1f12df63b677d6f
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-03-24 20:42:48 -0500

    Added in tank controls for levels

--- Lab 5/Assets/Scripts/PlayerLevelSteer.cs
+++ Lab 5/Assets/Scripts/PlayerLevelSteer.cs
@@ -2,15 +2,51 @@ using UnityEngine;
 
 public class PlayerLevelSteer : MonoBehaviour
 {
+    public float rotationSpeed;
+    float rotation;
+    public float directionalSpeed;
+    float push;
+    float accelerationBuffer;
+    public float accelerationFactor;
+    Rigidbody2D self;
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        accelerationBuffer = 0;
+        rotation = 0;
+        self = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rotation += Input.GetAxisRaw("Vertical") * rotationSpeed;
+        if (rotation > 90) {
+            rotation = 90;
+        }
+        if (rotation < -90) {
+            rotation = -90;
+        }
+        self.transform.eulerAngles = Vector3.forward * rotation;
+        push = Input.GetAxisRaw("Horizontal") * directionalSpeed;
+        if (push != 0) {
+            if (accelerationBuffer < 1) {
+                accelerationBuffer += accelerationFactor;
+            }
+            else {
+                accelerationBuffer = 1;
+            }
+            push *= accelerationBuffer;
+            self.transform.position += (self.transform.right * push);
+            Debug.Log(push + " " + accelerationBuffer + " " + accelerationFactor);
+            
+        }
+        else {
+            accelerationBuffer = 0;
+        }
+
+
     }
 }
