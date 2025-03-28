829d9650452ee2f90902071daa254f360fc551f1
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-03-28 18:12:12 -0500

    Game Manager Base Implemented
    Includes shell for dialogue implementation

--- Lab 5/Assets/Scripts/Open.cs
+++ Lab 5/Assets/Scripts/Open.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Open : MonoBehaviour
 {
     public bool opened;
+    public int requiredWeight;
+    GameManager manager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        manager = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -15,7 +17,8 @@ public class Open : MonoBehaviour
     }
 
     public void SetOpen() {
+        if (requiredWeight <= manager.playerWeight){
         opened = true;
-        Debug.Log("Crack!");
+        Debug.Log("Crack!");}
     }
 }
