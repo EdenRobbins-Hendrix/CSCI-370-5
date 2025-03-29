f18d5cedbfddac0652b8f1adc4db0fc7596570f3
Colten Berry <berrycj@hendrix.edu>
2025-03-29 02:27:58 -0500

    Prey animation looks better

--- Lab 5/Assets/Scripts/AvoidPlayer.cs
+++ Lab 5/Assets/Scripts/AvoidPlayer.cs
@@ -6,16 +6,19 @@ public class AvoidPlayer : MonoBehaviour
 {
 
     public float speed;
+    public float rotationSpeed;
     public GameObject target;
     // public GameObject dog;
     public static float minDist;
 
     private bool tagged;
     private Rigidbody2D body;
+    SpriteRenderer spriteRenderer;
 
     // Use this for initialization
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         body = GetComponent<Rigidbody2D>();
         minDist = 2.0f;
     }
@@ -32,9 +35,27 @@ public class AvoidPlayer : MonoBehaviour
             if (desired.magnitude < minDist)
             {
                 print("avoiding seal?");
+                float angle = (Mathf.Atan2(desired.y, desired.x) * Mathf.Rad2Deg);
+                Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    q, Time.deltaTime * rotationSpeed);
+
+
                 float actual = desired.magnitude - minDist;
                 body.AddForce(desired.normalized *
                     actual * speed - body.linearVelocity);
+                if (desired.x < 0)
+                {
+                    // spriteRenderer.flipX = true;
+                    spriteRenderer.flipY = true;
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    // spriteRenderer.flipX = false;
+                    spriteRenderer.flipY = false;
+                    spriteRenderer.flipX = true;
+                }
             }
         }
 
