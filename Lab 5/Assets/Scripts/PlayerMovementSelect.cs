957b90378c0480179105fc43b88b94b8cf7038a7
Fin <lockertfin@gmail.com>
2025-04-01 02:16:42 -0500

    Movement + animation, follow behaviors slightly broken

--- Lab 5/Assets/Scripts/PlayerMovementSelect.cs
+++ Lab 5/Assets/Scripts/PlayerMovementSelect.cs
@@ -1,41 +1,96 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovementSelect : MonoBehaviour
 {
+    public Animator animator;
+
+    // public ParticleSystem dust;
+
+    Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+
     float horizontal;
-    float vertical;
-    Rigidbody2D self;
-    Animator animator;
-    SpriteRenderer spriteRenderer;
-    public float speed;
+
+    public float runSpeed = 3f;
+    private bool m_Grounded;
+
+    public int dir = 1;
+    public int newDir = 1;
+
+    public UnityEvent OnLandEvent;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void StartSelect()
+    void Start()
     {
-        self = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        // if (OnLandEvent == null) {
+		//     OnLandEvent = new UnityEvent();
+        // }
+        // OnLandEvent.AddListener(Landed);
     }
 
     // Update is called once per frame
-    void FixedUpdateSelect()
+    void Update()
     {
-        //Replace this with animator based movement later on
-        vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
         if (horizontal < 0) {
             spriteRenderer.flipX = true;
-        } else if (horizontal > 0) {
+            newDir = 1;
+        } 
+        if (horizontal > 0) {
             spriteRenderer.flipX = false;
+            newDir = 0;
         }
-        if (vertical != 0) {
-            animator.SetBool("VerticalMove", true);
-        }
-        else {
-            animator.SetBool("VerticalMove", false);
-        }
-        animator.SetFloat("Horizontal", horizontal);
 
-        self.linearVelocity = new Vector2(horizontal, vertical);
+        // if (dir != newDir) { // Determines if direction changes, plays dust
+        //     dir = newDir;
+        //     createDust();
+        // }
+
+        if (Input.GetKeyDown("space") && m_Grounded)
+        {
+            // createDust();
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * 300);
+            // animator.SetBool("IsJumping", true);
+            // Debug.Log("space key was pressed");
+        }
+
     }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().linearVelocity = new Vector2(horizontal * runSpeed, GetComponent<Rigidbody2D>().linearVelocity.y);
+    
+		bool wasGrounded = m_Grounded;
+;
+        m_Grounded = false;
+
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.2f);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+                m_Grounded = true;
+				if (!wasGrounded)
+					OnLandEvent.Invoke();
+			}
+		}
+	}
+
+    // public void Landed() {
+    //     // createDust();
+    //     animator.SetBool("IsJumping", false);
+    // }
+
+    // void createDust() {
+    //     dust.Play();
+    // }
 }
