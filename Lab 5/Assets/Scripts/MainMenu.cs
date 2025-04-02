f8890d7887a8689bf10d67d515c321e64dea8b89
Fin <lockertfin@gmail.com>
2025-04-02 02:14:35 -0500

    Buttons make noise

--- Lab 5/Assets/Scripts/MainMenu.cs
+++ Lab 5/Assets/Scripts/MainMenu.cs
@@ -3,10 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
+    [SerializeField] private AudioClip buttonClickSound;
+
     public void PlayGame() 
     {
         SceneManager.LoadScene("Game");
     }
 
+    public void ButtonPressed()
+    {
+        SoundFXManager.instance.PlayButtonClick(buttonClickSound, transform, 1f);
+    }
 }
