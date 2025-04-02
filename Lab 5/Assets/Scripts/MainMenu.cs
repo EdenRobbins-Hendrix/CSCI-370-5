a9aa7584384f819367aeb2c8cfbbab88428d78a2
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-04-02 16:02:17 -0500

    Revert "Functioning options/volume menu (just have to add in the actual SFX)"
    This reverts commit 219b3262a1dc06bc96f65141bb0c488b4a60e02b, reversing
    changes made to 86c0cad1750b91d282f3075cd89ae99ee2fdd53d.

--- Lab 5/Assets/Scripts/MainMenu.cs
+++ Lab 5/Assets/Scripts/MainMenu.cs
@@ -3,18 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    [SerializeField] private AudioClip buttonClickSound;
-
-    public SoundMixerManager soundMixerManager;
-
+    
     public void PlayGame() 
     {
         SceneManager.LoadScene("Game");
     }
 
-    public void ButtonPressed()
-    {
-        SoundFXManager.instance.PlayButtonClick(buttonClickSound, transform, 1f);
-    }
 }
