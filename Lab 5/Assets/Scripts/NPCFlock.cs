0445a14492c4a25b6ebe0e6402e6af24d8699dc9
EdenRobbins-Hendrix <eerobbins82@gmail.com>
2025-04-01 21:30:23 -0500

    Added in movement SFX

--- Lab 5/Assets/Scripts/NPCFlock.cs
+++ Lab 5/Assets/Scripts/NPCFlock.cs
@@ -12,9 +12,11 @@ public class NPCFlock : MonoBehaviour
     Animator animator;
     SpriteRenderer sprite;
     public bool moving;
+    public AudioSource SFX;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SFX.Pause();
         changeWait();
         moving = true;
         sprite = GetComponent<SpriteRenderer>();
@@ -33,13 +35,29 @@ public class NPCFlock : MonoBehaviour
     {   changeWait();
         if (transform.position.x != goal) {
             if (goal < transform.position.x) {
+                SFX.UnPause();
                 sprite.flipX = true;
             }
             else if (goal > transform.position.x){
+                SFX.UnPause();
                 sprite.flipX = false;
+            }
+            else {
+                SFX.Pause();
             }
             animator.SetBool("Moving", true);
             transform.position = Vector2.MoveTowards(transform.position, new Vector2 (goal, transform.position.y), speed);
+            if (timer <= 0) {
+                timer = maxTime + waitVariability;
+                PickSpot();
+            }
+            else {
+                float decrement = 0.05f * radius/speed*2;
+                if (decrement > 0.5f) {
+                    decrement = 0.5f;
+                } 
+                timer -= decrement;
+            }
         }
         else {
             animator.SetBool("Moving", false);
@@ -54,7 +72,8 @@ public class NPCFlock : MonoBehaviour
 
     void PickSpot(){
         float circle = Random.Range(-1, 1) * radius;
+        circle += (radius/2);
         goal = circle + player.transform.position.x;
-        Debug.Log("The goal is: " + goal);
+        Debug.Log("The goal is: " + goal + ". The base is " + circle);
     }
 }
